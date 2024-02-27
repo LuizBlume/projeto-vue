2b7e33ca321ae36db26718aeaa99af04508b35e8
LuizBlume <luiz.fernando.251107@gmail.com>
2024-02-27 12:51:47 -0300

    hotfix: Adição de script setup.

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,26 @@
-<script>
- import { ref } from 'vue';
-
-  const contador = ref(0);
-
-  function incrementar() {
-    contador.value++;
+<script setup>
+import { ref } from 'vue'
+const contador = ref(0)
+function incrementar() {
+  contador.value++;
+}
+function decrementar() {
+  if (contador.value > 0) {
+    contador.value--;
   }
-
-  function decrementar() {
-    if (contador.value > 0) {
-      contador.value--;
-    }
-  }
-
-  function reiniciar() {
-    contador.value = 0;
-  }
+}
+function reiniciar() {
+  contador.value = 0
+}
 </script>
-
 <template>
-  <button @click="incrementar++">+</button>
+  <button @click="incrementar">+</button>
   <button @click="decrementar">-</button>
   <button @click="reiniciar">zerar</button>
-  <p>Valor do contador: {{  contador }}</p>
+  <p>Valor do contador: {{ contador }}</p>
 </template>
-
 <style scoped>
-button{
+button {
   background-color: black;
   color: aliceblue;
   margin: 5pxe;
